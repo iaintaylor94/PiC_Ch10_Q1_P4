ad67c4ba3c8f7910c988f22d51eac063bff2d83c
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-19 13:25:59 +0000

    copied program

--- .ccls-cache/@home@runner@Ch10Q1P4-testing-strings-for-equality/main.c
+++ .ccls-cache/@home@runner@Ch10Q1P4-testing-strings-for-equality/main.c
@@ -1,6 +1,37 @@
+// Program to test if two strings are equal
+
 #include <stdio.h>
+#include <stdbool.h>
+
+// Declare Functions
+bool areEqual (const char [], const char []);
 
 int main(void) {
-  printf("Hello World\n");
+
+  // Define Strings
+  const char s1[] = "string compare test";
+  const char s2[] = "string";
+
+  printf ("%d\n", areEqual(s1, s2));
+  printf ("%d\n", areEqual(s1, s1));
+  printf ("%d\n", areEqual(s2, "string"));
+  
   return 0;
+}
+
+// Define Functions
+bool areEqual (const char str1[], const char str2[]) {
+  int i = 0;
+  bool equal;
+
+  while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0') {
+    i++;
+  }
+
+  if (str1 [i] == '\0' && str2 [i] == '\0')
+    equal = true;
+  else
+    equal = false;
+
+  return equal;
 }
